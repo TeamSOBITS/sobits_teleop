9dc86eb3da887f8186af7078628056a50f17d649
apparettedare <ksuzuki9541@gmail.com>
2025-11-28 03:10:13 +0900

    [ADD] install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "╔══╣ Setup: SOBITS TELEOP (STARTING) ╠══╗"
+
+
+# Keep track of the current directory
+DIR=`pwd`
+cd ..
+
+# Download required packages
+ros_packages=(
+    "keyboard_joy"
+    "ROS-TCP-Endpoint"
+)
+
+#Clone all packages
+for ((i = 0; i < ${#ros_packages[@]}; i++)) {
+    echo "Clonning: ${ros_packages[i]}"
+    git clone --recurse-submodules -b $ROS_DISTRO-devel https://github.com/TeamSOBITS/${ros_packages[i]}.git
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
+}
+
+# Download ROS packages
+sudo apt-get update
+sudo apt-get install -y \
+    ros-$ROS_DISTRO-joy-linux
+
+# Go back to previous directory
+cd ${DIR}
+
+echo "╚══╣ Setup: SOBITS TELEOP (FINISHED) ╠══╝"
